eb22d27e360aa22df7087c26b58f3e7f8e71ed0f
evelisko <37504765+evelisko@users.noreply.github.com>
2019-11-26 12:18:11 +0400

    Добавлена обработка события по приему данных.

---
+++ app/src/main/java/com/example/json_parser/CompanyAdapter.java
@@ -0,0 +1,58 @@
+package com.example.json_parser;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+// import android.widget.ListView;
+
+import java.util.List;
+
+public class CompanyAdapter extends ArrayAdapter<Company> {
+
+  private LayoutInflater inflater;
+  private int layout;
+  private List<Company>  companies;
+// private ArrayAdapter<>
+
+  public CompanyAdapter(Context context, int resource, List<Company> companies) {
+     super(context, resource, companies);
+     this.companies = companies;
+     this.layout = resource;
+     this.inflater = LayoutInflater.from(context);
+  }
+
+  public View getView(int position, View convertView, ViewGroup parent) {
+      ViewHolder viewHolder;
+      if(convertView== null){
+          convertView = inflater.inflate(this.layout, parent, false);
+          viewHolder = new ViewHolder(convertView);
+          convertView.setTag(viewHolder);
+      }
+      else{
+          viewHolder = (ViewHolder) convertView.getTag();
+      }
+
+     Company company = companies.get(position);
+     viewHolder.companyNameView.setText(company.getName());
+     viewHolder.companyAgeView.setText("Возраст компанни: "+ company.getAge());
+     viewHolder.companyCompetencesView.setText("Область деятельности: \n    "+company.getCompetencesString());
+
+     viewHolder.employeeView.setText(company.getEmployeeString());
+    return convertView;
+  }
+
+    private class ViewHolder {
+         final TextView companyNameView, companyAgeView,companyCompetencesView;
+         final TextView employeeView;
+        ViewHolder(View view) {
+            companyNameView = (TextView)view.findViewById(R.id.company_name);
+            companyAgeView = (TextView)view.findViewById(R.id.company_age);
+            companyCompetencesView = (TextView)view.findViewById(R.id.company_competences);
+            employeeView = (TextView)view.findViewById(R.id.employees);
+        }
+    }
+
+}
--- app/src/main/java/com/example/json_parser/MainActivity.java
+++ app/src/main/java/com/example/json_parser/MainActivity.java
@@ -1,36 +1,18 @@
 package com.example.json_parser;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.os.Bundle;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLPeerUnverifiedException;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import android.widget.Toast;
 import java.util.List;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.content.Context;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 // Если программа отказывается запускаться. нужно удалить приложение из эмулятора.
 // и перезапустить отладку.
@@ -39,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     ListView lvCompanies;
     TextView textView;
     JsonReader jsonReader;
+    Button btnOk;
+
     private String TAG = MainActivity.class.getSimpleName();
     private static final String FILE_NAME = "http://www.mocky.io/v2/56fa31e0110000f920a72134";
 
@@ -50,30 +34,53 @@ public class MainActivity extends AppCompatActivity {
         lvCompanies = (ListView) findViewById(R.id.listViewCompanies);
         textView = (TextView) findViewById(R.id.textView);
 
-        jsonReader = new JsonReader();
-
-            if (!isOnline())
+        btnOk = (Button) findViewById(R.id.btnOk);
+//        // создание обработчика
+        OnClickListener oclBtnOk = new OnClickListener() {
+            @Override
+            public void onClick(View v)
             {
-              Toast.makeText(this, "Нет подключения к сети", Toast.LENGTH_LONG).show();
-              Log.e("MayTage", "Нет подключения к сети");
+              // TODO Auto-generated method stub
+                Log.e("MayTage", "Нажата Ok 1");
             }
-            else
-            {
-               Log.e("MayTage", "Устройство подключено");
+        };
+        btnOk.setOnClickListener(oclBtnOk);
 
-               textView.append("\n Устройство подключено");
-            try {
-                  jsonReader.readCompanyJSONFile();
+//        public interface CommandDataAvailiableListener {
+//            public void commandDataAvailiable(CommandDataAvailiableEvent e);
+//        }
 
-             //   new GetCompanies().execute();
-
-                } catch (Exception e) {
-                    textView.append("\n Ошибка подключения "+e.getMessage());
-                    Log.e("MayTage", "Ошибка подключения");
-                    e.printStackTrace();
-                }
+       jsonReader = new JsonReader();
+//
+      JsonReader.CreateCompanyListListener createCompanyListListener = new JsonReader.CreateCompanyListListener() {
+         public void  onCreateCompanyListListener(List<Company> companies){
+             CreateCompanyList(companies);
          }
-   }
+        };
+        jsonReader.setCompanyListListener(createCompanyListListener);
+
+        if (!isOnline())
+        {
+            Toast.makeText(this, "Нет подключения к сети", Toast.LENGTH_LONG).show();
+            Log.e("MayTage", "Нет подключения к сети");
+        }
+        else
+        {
+            Log.e("MayTage", "Устройство подключено");
+
+            textView.append("\n Устройство подключено");
+            try {
+                jsonReader.readCompanyJSONFile();
+
+                //   new GetCompanies().execute();
+
+            } catch (Exception e) {
+                textView.append("\n Ошибка подключения "+e.getMessage());
+                Log.e("MayTage", "Ошибка подключения");
+                e.printStackTrace();
+            }
+        }
+  }
 
     public boolean isOnline() {
 //  String cs = this.CONNECTIVITY_SERVICE;
@@ -89,199 +96,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+ private void CreateCompanyList(List<Company>companyList)
+ {
+   try {
+         if (companyList != null) {
 
+             CompanyAdapter adapter = new CompanyAdapter(this, R.layout.company_layout, companyList);
+             lvCompanies.setAdapter(adapter);
+             textView.setText(companyList.get(0).getName());
 
-    /**
-     * Async task class to get json by making HTTP call
-     */
-    private class GetCompanies extends AsyncTask<Void, Void, List<Company>> {
+             Toast.makeText(this, "Данные считанны", Toast.LENGTH_LONG).show(); //аналог massegeBox
+         } else {
+             Toast.makeText(this, "Не удалось открыть данные", Toast.LENGTH_LONG).show();
+             textView.append("\n Не удалось открыть данные");
+         }
+     } catch (Exception e) {
+         textView.append("\n Ошибка подключения "+e.getMessage());
+         e.printStackTrace();
+     }
+ }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Showing progress dialog
-//            pDialog = new ProgressDialog(MainActivity.this);
-//            pDialog.setMessage("Please wait...");
-//            pDialog.setCancelable(false);
-//            pDialog.show();
-        }
-
-        @Override
-        protected List<Company>  doInBackground(Void... arg0) {
-
-            HttpHandler sh = new HttpHandler();
-//          Making a request to url and getting response
-            Log.e(TAG, "File Name url: " + FILE_NAME);
-            String jsonStr = sh.makeServiceCall(FILE_NAME);
-//
-            Log.e(TAG, "Response from url: " + jsonStr);
-
-            if (jsonStr != null) {
-                Log.e(TAG, "doInBackground JSON-str not NULL");
-//              Выполняем анализ полученной строки.
-                try {
-                    return importFromJSON(jsonStr);
-                }catch (JSONException ex)
-                {
-                    return null;
-                }
-//                try {
-//                    JSONObject jsonObj = new JSONObject(jsonStr);
-//
-//                    // Getting JSON Array node
-//                    JSONArray contacts = jsonObj.getJSONArray("contacts");
-//
-//                    // looping through All Contacts
-//                    for (int i = 0; i < contacts.length(); i++) {
-//                        JSONObject c = contacts.getJSONObject(i);
-//
-//                        String id = c.getString("id");
-//                        String name = c.getString("name");
-//                        String email = c.getString("email");
-//                        String address = c.getString("address");
-//                        String gender = c.getString("gender");
-//
-//                        // Phone node is JSON Object
-//                        JSONObject phone = c.getJSONObject("phone");
-//                        String mobile = phone.getString("mobile");
-//                        String home = phone.getString("home");
-//                        String office = phone.getString("office");
-//
-//                        // tmp hash map for single contact
-//                        HashMap<String, String> contact = new HashMap<>();
-//
-//                        // adding each child node to HashMap key => value
-//                        contact.put("id", id);
-//                        contact.put("name", name);
-//                        contact.put("email", email);
-//                        contact.put("mobile", mobile);
-//
-//                        // adding contact to contact list
-//                        contactList.add(contact);
-//                    }
-//                } catch (final JSONException e) {
-//                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            Toast.makeText(getApplicationContext(),
-//                                    "Json parsing error: " + e.getMessage(),
-//                                    Toast.LENGTH_LONG)
-//                                    .show();
-//                        }
-//                    });
-
-//                }
-//            } else {
-///////////////////////////////////////////////////////////////////////////////////////////////////
-//                Log.e(TAG, "Couldn't get json from server.");
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(getApplicationContext(),
-//                                "Couldn't get json from server. Check LogCat for possible errors!",
-//                                Toast.LENGTH_LONG)
-//                                .show();
-//        //            }
-//        //        });
-////////////////////////////////////////////////////////////////////////////////////////////////////
-            }
-            else
-            {
-                return null;
-            }
-
-        }
-
-        @Override
-        protected void onPostExecute(List<Company> result) {
-            super.onPostExecute(result);
-            Log.e(TAG, "Asinch is finish");
-        }
-
-//            И вот сюда же добавим обработку запроса.
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-        private List<Company> importFromJSON(String jString) throws JSONException{
-
-            InputStreamReader streamReader = null;
-            FileInputStream fileInputStream = null;
-            List<Company> comps;
-
-            try {
-                comps = new ArrayList<>();
-
-                JSONObject jsonRoot = new JSONObject(jString);
-
-                for(int s = 0; s <  jsonRoot.length();s++)
-                {
-                    // считываем пока не будет достигнут конец файла.
-                    JSONObject jcmp = jsonRoot.getJSONObject("company");
-
-                    JSONArray jsonArray = jcmp.getJSONArray("competences");
-
-                    String[] competences = new String[jsonArray.length()];
-
-                    for (int i = 0; i < jsonArray.length(); i++)
-                    {
-                        competences[i] = jsonArray.getString(i);
-                    }
-
-                    JSONArray jsonArray1 = jcmp.getJSONArray("employees");
-                    Employee[] employees = new Employee[jsonArray1.length()];
-
-                    for (int i = 0; i < jsonArray1.length(); i++)
-                    {
-                        Employee emp = new Employee();
-
-                        JSONObject jEmployee = jsonArray1.getJSONObject(i);
-                        String str = jEmployee.getString("name");
-                        emp.setName(str);
-
-                        emp.setPhone_number(jEmployee.getString("phone_number"));
-                        JSONArray jSkills = jEmployee.getJSONArray("skills");
-
-                        String[] skills = new String[jSkills.length()];
-                        for (int j = 0; j < jSkills.length(); j++) {
-                            skills[j] = jSkills.getString(j);
-                        }
-                        emp.setSkills(skills);
-
-                        employees[i] = emp;
-                    }
-
-                    Company cmp = new Company(jcmp.getString("name"),jcmp.getString("age"),competences);
-                    cmp.setEmployee(employees);
-                    comps.add(cmp);
-                }
-//------------------------------------------------------------------------------------------------//
-
-                //       } catch (IOException ex)
-                //      {
-                //           ex.printStackTrace();
-                //           return null;
-            } finally {
-                if (streamReader != null) {
-                    try {
-                        streamReader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (fileInputStream != null) {
-                    try {
-                        fileInputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            return comps;
-        }
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-    }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
